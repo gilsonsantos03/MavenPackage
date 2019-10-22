c8b1d56a870150339e0b8984e3fa937c36e3d007
Gilson <gilsonsantos1998@gmail.com>
2019-10-21 22:55:20 -0300

    classe com a main

---
+++ src/main/java/ufma/ecp/paradigmas/maven_projects/CadastraContato.java
@@ -0,0 +1,17 @@
+package ufma.ecp.paradigmas.maven_projects;
+
+import ufma.ecp.paradigmas.maven_projects.JanelaCadastro;
+
+public class CadastraContato {
+
+	public static void main(String[] args) {
+		//ContatoDAO c1 = new ContatoDAO();
+				//Contato contato = new Contato("Gilson","gilsonsantos@gmail.com", "81433543");
+				//c1.adicionaContato(contato);
+				//System.out.println(c1.recuperaContato());
+				
+				JanelaCadastro j = new JanelaCadastro();
+				j.setVisible(true);
+	}
+
+}
